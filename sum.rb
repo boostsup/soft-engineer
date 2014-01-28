f43f178b0b8f89d77f93c4d6613f31af0d505807
Joseph Zhu <zhubujun314@gmail.com>
2014-01-27 21:56:18 -0500

    second commit

--- sum.rb
+++ sum.rb
@@ -1,8 +1,9 @@
-class Array
-    def sum
-		return 0 if self.empty?
-		self.inject{|sum,x| sum + x }
-    end
-end
+#Define a method sum which takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
 
-puts [1,3,4].sum
+    def sum(arr)
+		return 0 if arr.empty?
+		arr.inject{|sum,x| sum + x }
+    end
+
+puts sum([1,3,4])
+puts sum([])
